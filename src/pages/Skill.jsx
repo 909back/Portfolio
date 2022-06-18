4b323df6256778e18187b233fff0b0592b492e1d
ejbaek <909back@naver.com>
2022-06-18 22:03:05 +0900

    [UPDATE] 2022/06/17
    1. 스킬 수정
    2. 프로젝트 내용 수정

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "../assets/scss/Skill.module.scss";
 import Level from "../components/Level";
 import SkillItem from "../components/SkillItem";
-import SkillSideBar from '../components/SkillSideBar';
+import SkillSideBar from "../components/SkillSideBar";
 
 const Skill = () => {
   return (
@@ -10,7 +10,7 @@ const Skill = () => {
       <section className={style.page}>
         <article className={style.container}>
           <div className={[style.skillHeader, style.frontend].join(" ")}>
-            <h2>Frontend</h2>
+            <h2>Main</h2>
           </div>
 
           <ul className={style.skillList}>
@@ -62,7 +62,7 @@ const Skill = () => {
 
         <article className={style.container}>
           <div className={[style.skillHeader, style.backend].join(" ")}>
-            <h2>Backend</h2>
+            <h2>ETC</h2>
           </div>
 
           <ul className={style.skillList}>
@@ -99,21 +99,20 @@ const Skill = () => {
               </svg>
             </SkillItem>
 
-            <SkillItem name="GraphQL" level="L" styleName={style.graphql}>
+            <SkillItem name="TypeScirpt" level="L" styleName={style.type}>
               <svg
                 role="img"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <title>GraphQL</title>
-                <path d="M12.002 0a2.138 2.138 0 1 0 0 4.277 2.138 2.138 0 1 0 0-4.277zm8.54 4.931a2.138 2.138 0 1 0 0 4.277 2.138 2.138 0 1 0 0-4.277zm0 9.862a2.138 2.138 0 1 0 0 4.277 2.138 2.138 0 1 0 0-4.277zm-8.54 4.931a2.138 2.138 0 1 0 0 4.276 2.138 2.138 0 1 0 0-4.276zm-8.542-4.93a2.138 2.138 0 1 0 0 4.276 2.138 2.138 0 1 0 0-4.277zm0-9.863a2.138 2.138 0 1 0 0 4.277 2.138 2.138 0 1 0 0-4.277zm8.542-3.378L2.953 6.777v10.448l9.049 5.224 9.047-5.224V6.777zm0 1.601 7.66 13.27H4.34zm-1.387.371L3.97 15.037V7.363zm2.774 0 6.646 3.838v7.674zM5.355 17.44h13.293l-6.646 3.836z" />
+                <title>TypeScript</title>
+                <path d="M1.125 0C.502 0 0 .502 0 1.125v21.75C0 23.498.502 24 1.125 24h21.75c.623 0 1.125-.502 1.125-1.125V1.125C24 .502 23.498 0 22.875 0zm17.363 9.75c.612 0 1.154.037 1.627.111a6.38 6.38 0 0 1 1.306.34v2.458a3.95 3.95 0 0 0-.643-.361 5.093 5.093 0 0 0-.717-.26 5.453 5.453 0 0 0-1.426-.2c-.3 0-.573.028-.819.086a2.1 2.1 0 0 0-.623.242c-.17.104-.3.229-.393.374a.888.888 0 0 0-.14.49c0 .196.053.373.156.529.104.156.252.304.443.444s.423.276.696.41c.273.135.582.274.926.416.47.197.892.407 1.266.628.374.222.695.473.963.753.268.279.472.598.614.957.142.359.214.776.214 1.253 0 .657-.125 1.21-.373 1.656a3.033 3.033 0 0 1-1.012 1.085 4.38 4.38 0 0 1-1.487.596c-.566.12-1.163.18-1.79.18a9.916 9.916 0 0 1-1.84-.164 5.544 5.544 0 0 1-1.512-.493v-2.63a5.033 5.033 0 0 0 3.237 1.2c.333 0 .624-.03.872-.09.249-.06.456-.144.623-.25.166-.108.29-.234.373-.38a1.023 1.023 0 0 0-.074-1.089 2.12 2.12 0 0 0-.537-.5 5.597 5.597 0 0 0-.807-.444 27.72 27.72 0 0 0-1.007-.436c-.918-.383-1.602-.852-2.053-1.405-.45-.553-.676-1.222-.676-2.005 0-.614.123-1.141.369-1.582.246-.441.58-.804 1.004-1.089a4.494 4.494 0 0 1 1.47-.629 7.536 7.536 0 0 1 1.77-.201zm-15.113.188h9.563v2.166H9.506v9.646H6.789v-9.646H3.375z" />
               </svg>
             </SkillItem>
           </ul>
         </article>
 
-        <SkillSideBar/>
-
+        <SkillSideBar />
       </section>
     </>
   );
